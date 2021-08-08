43eeb90c9ff0c30f2b2d4303459a1689e4b55268
kamesdev <kamilpienkos1@gmail.com>
2021-08-08 20:28:59 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pablo-ip
+Hello guys
---
+++ app.js
@@ -0,0 +1,39 @@
+// Create ips row in LS if there is not already
+if (!localStorage.getItem('ips')) {
+  localStorage.setItem('ips', JSON.stringify([]))
+}
+
+fetch('https://api.db-ip.com/v2/free/self')
+
+  .then(res => res.json())
+  
+    .then(
+
+      data => {
+        // current ip
+        const currentIp = data.ipAddress
+
+        const ips = JSON.parse(localStorage.getItem('ips'))
+
+        const respond = document.querySelector('.respond')
+
+        // check if ip is already in LS
+        if (ips.includes(currentIp)) {
+
+          respond.classList.add('error')
+          respond.textContent = '🚫 NIE :('
+
+        } else {
+
+          respond.classList.add('success')
+          respond.textContent = '✅ TAK :)'
+          ips.push(currentIp)
+
+        }
+
+        localStorage.setItem('ips', JSON.stringify(ips))
+      }
+      
+      )
+
+
